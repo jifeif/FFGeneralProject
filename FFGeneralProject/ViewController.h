96cbfc89e8ec2694a72415c4a91ffe2bddb7d26b
吉飞飞 <2326439727@qq.com>
2019-07-24 09:46:39 +0800

    Initial Commit

---
+++ FFGeneralProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FFGeneralProject
+//
+//  Created by jisa on 2019/7/24.
+//  Copyright © 2019 jff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FFGeneralProject/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  FFGeneralProject
+//
+//  Created by jisa on 2019/7/24.
+//  Copyright © 2019 jff. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
